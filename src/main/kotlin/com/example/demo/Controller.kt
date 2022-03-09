9ea64cd98117da8115aac1d9d47e2f7efc65f9ef
Leejjon <leejjon@gmail.com>
2022-03-09 16:35:20 +0100

    Before feign

--- src/main/kotlin/com/example/demo/Controller.kt
+++ src/main/kotlin/com/example/demo/Controller.kt
@@ -2,21 +2,21 @@ package com.example.demo
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.env.Environment
-import org.springframework.core.env.get
+import org.springframework.core.env.ConfigurableEnvironment
 import org.springframework.ui.Model
-import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class Controller(@Value("\${iets.met.punten}") val hello: String) {
-    @Autowired
-    private val environment: Environment? = null
-
+class Controller @Autowired constructor(@Value("\${iets.met.punten}") val hello: String, val environment: ConfigurableEnvironment) {
     @GetMapping("/")
     fun blog(model: Model): String {
+        environment.propertySources
+                .forEach {
+                    println("Source: " + it.source)
+                    println("Iets met punten: " + it.getProperty("iets.met.punten"))
+                }
         return hello
     }
 }
